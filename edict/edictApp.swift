fa4d2b62ca7215f0e7296d3d6d493e01a790f3c8
byzhao <381016296@qq.com>
2024-07-26 00:12:37 +0800

    Initial Commit

---
+++ edict/edictApp.swift
@@ -0,0 +1,17 @@
+//
+//  edictApp.swift
+//  edict
+//
+//  Created by Mac on 2024/7/26.
+//
+
+import SwiftUI
+
+@main
+struct edictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
